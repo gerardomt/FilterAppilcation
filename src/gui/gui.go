a82bd8c5f43d3d030daa8f00d9c366b391ca3334
gerardomt <gerardomt@ciencias.unam.mx>
2019-09-04 01:07:27 -0500

    File options implemented

--- src/gui/gui.go
+++ src/gui/gui.go
@@ -1,11 +1,17 @@
 package gui
 
 import (
+	"os"
+	"path"
+	"filter"
 	"github.com/mattn/go-gtk/glib"
 	"github.com/mattn/go-gtk/gtk"
+	"github.com/mattn/go-gtk/gdkpixbuf"
 )
 
 func LoadImage(filename string, image *gtk.Image){
+	pixx, _ := gdkpixbuf.NewPixbufFromFileAtScale(filename, 600, 600, true)
+    image.SetFromPixbuf(pixx)
 }
 
 func InitWindow() {
@@ -23,7 +29,18 @@ func InitWindow() {
 	menubar := gtk.NewMenuBar()
 	vbox.PackStart(menubar, false, false, 0)
 
-	//MENÚS 
+	//Carga una imagen por default
+	dir, _ := path.Split(os.Args[0])
+	imagefile := path.Join(dir, "../media/fotitus.jpg")
+		
+	pixx, _ := gdkpixbuf.NewPixbufFromFileAtScale(imagefile, 600, 600, true)
+	image := gtk.NewImageFromPixbuf(pixx)
+
+	imageFilter, _ := filter.NewFilter(imagefile)
+	
+	vbox.Add(image)
+
+	//MENÚ
 	//Menú de archivos
 	filemenu := gtk.NewMenuItemWithMnemonic("_File")
 	menubar.Append(filemenu)
@@ -33,10 +50,55 @@ func InitWindow() {
 
 	//Abrir archivo
 	openFileItem := gtk.NewMenuItemWithMnemonic("_Open")
+		openFileItem.Connect("activate", func(){
+		filechooserdialog := gtk.NewFileChooserDialog(
+			"Choose File...",
+			openFileItem.GetTopLevelAsWindow(),
+		       	gtk.FILE_CHOOSER_ACTION_OPEN,
+	       		gtk.STOCK_OK,
+	       		gtk.RESPONSE_ACCEPT)
+ 		filter := gtk.NewFileFilter()
+	   	filter.AddPattern("*.jpg")
+	   	filechooserdialog.AddFilter(filter)
+		filechooserdialog.Response(func() {
+			filePath := filechooserdialog.GetFilename()
+			LoadImage(filePath, image)
+		   	imageFilter.SetImage(filePath)
+	       		filechooserdialog.Destroy()
+		})
+		filechooserdialog.Run()
+	})
 	subfilemenu.Append(openFileItem)
 
 	//Guardar archivo
 	saveFileItem := gtk.NewMenuItemWithMnemonic("_Save")
+	saveFileItem.Connect("activate", func(){
+		filechooserdialog := gtk.NewFileChooserDialog(
+			"Save File...",
+			saveFileItem.GetTopLevelAsWindow(),
+			gtk.FILE_CHOOSER_ACTION_SAVE,
+			gtk.STOCK_SAVE,
+			gtk.RESPONSE_ACCEPT)
+		filechooserdialog.Response(func() {
+			filePath := filechooserdialog.GetFilename()
+			_, err := imageFilter.SaveImageAt(filePath)
+			if err != nil {
+				messagedialog := gtk.NewMessageDialog(
+					filechooserdialog.GetTopLevelAsWindow(),
+					gtk.DIALOG_MODAL,
+					gtk.MESSAGE_INFO,
+					gtk.BUTTONS_OK,
+					"No filter applied",
+				)
+				messagedialog.Response(func() {
+					messagedialog.Destroy()
+				})
+				messagedialog.Run()
+			}
+			filechooserdialog.Destroy()
+		})
+		filechooserdialog.Run()
+	})
 	subfilemenu.Append(saveFileItem)
 
 	//Salir
@@ -55,22 +117,66 @@ func InitWindow() {
 
 	//Filtro Azul
 	blueFilterItem := gtk.NewMenuItemWithMnemonic("_Blue")
+	blueFilterItem.Connect("activate", func(){
+		imageFilter.BlueFilter()
+		path, _ := imageFilter.SaveImage("blue")
+		LoadImage(path, image)
+	})
 	subfiltermenu.Append(blueFilterItem)
 
 	//Filtro Rojo
 	redFilterItem := gtk.NewMenuItemWithMnemonic("_Red")
+	redFilterItem.Connect("activate", func(){
+		imageFilter.RedFilter()
+		path, _ := imageFilter.SaveImage("red")
+		LoadImage(path, image)
+	})
 	subfiltermenu.Append(redFilterItem)
 
 	//Filtro Verde
 	greenFilterItem := gtk.NewMenuItemWithMnemonic("_Green")
+	greenFilterItem.Connect("activate", func(){
+		imageFilter.GreenFilter()
+		path, _ := imageFilter.SaveImage("green")
+		LoadImage(path, image)
+	})
 	subfiltermenu.Append(greenFilterItem)
 
 	//Filtro Gris
 	greyFilterItem := gtk.NewMenuItemWithMnemonic("_Grey")
+	greyFilterItem.Connect("activate", func(){		
+		imageFilter.GreyFilter()
+		path, _ := imageFilter.SaveImage("grey")
+		LoadImage(path, image)
+	})
 	subfiltermenu.Append(greyFilterItem)
 
 	//Filtro Mosaico
 	pixelItem := gtk.NewMenuItemWithMnemonic("_Pixelation")
+	pixelItem.Connect("activate", func(){
+		var size int
+		subwindow := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+		subwindow.SetPosition(gtk.WIN_POS_CENTER)
+		subwindow.SetTitle("Big Pixel Size")
+
+		subbox :=gtk.NewVBox(false, 1)
+		label :=gtk.NewLabel("Select the size of the pixel")
+		subbox.Add(label)
+		scale := gtk.NewHScaleWithRange(10, 100, 5)
+		subbox.Add(scale)
+		button := gtk.NewButtonWithLabel("Create")
+		button.Clicked(func(){
+			size = int(scale.GetValue())
+			subwindow.Destroy()
+			imageFilter.PixelFilter(size)
+			path, _ := imageFilter.SaveImage("pixel")
+			LoadImage(path, image)
+		})
+		subbox.Add(button)
+		subwindow.Add(subbox)
+		subwindow.SetSizeRequest(300, 150)
+		subwindow.ShowAll()
+	})
 	subfiltermenu.Append(pixelItem)
 
 	window.Add(vbox)
